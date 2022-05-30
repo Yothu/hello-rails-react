1c84211635408dbb4d4c968dfd3559031f3f5183
David Vergaray <david.vergaray.a@gmail.com>
2022-05-30 12:29:57 -0400

    Create Api endpoint for greetings

---
+++ app/controllers/v1/greetings_controller.rb
@@ -0,0 +1,13 @@
+class V1::GreetingsController < ApplicationController
+  def index
+    greeting = Greeting.find(rand(1..Greeting.count))
+
+    render json: { :greetings => [
+      {
+        :message => greeting.message
+      }
+    ]
+
+  }.to_json
+  end
+end
